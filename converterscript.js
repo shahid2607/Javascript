651ef76570b5f26ab4cddede70ce6e538fffbcf3
shahid2607 <sa3260445@gmail.com>
2022-12-19 17:32:45 +0530

    New

---
+++ converterscript.js
@@ -0,0 +1,26 @@
+//  var fistNumber=parseInt(prompt("Enter the first number"));
+//  var secondNumber=parseInt(prompt("enter the second number"));
+
+// result=fistNumber+secondNumber
+// sub=fistNumber-secondNumber;
+// div=fistNumber/secondNumber;
+
+// alert(result,sub,div)
+var speedInKmph=parseInt(prompt("Enter the value in KMPH"));
+
+ mph=speedInKmph*.621
+ meterPerSec=speedInKmph*.277
+
+ document.writeln("Speed in Miles per hour"+ mph +"<br>"+"Speed in Meter per secomd"+meterPerSec)
+ 
+
+ alert("Spped in Miles per hour"+ mph )
+ alert("Speed in Meter per secomd"+meterPerSec) 
+
+
+ console.log("Speed in Miles per hour"+ mph)
+ console.log("Speed in meter per second"+ meterPerSec)
+ 
+ 
+
+
